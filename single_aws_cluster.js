576f3a6ab1b8bd87c74a3128a2ef092a3e1782b7
Patrice-Morgan Ongoly <ongoly.patricemorgan@gmail.com>
2020-03-16 20:48:33 -0400

    upgrade animations to follow aframe's event emitter functionality instead of the animation component's delay attribute

--- single_aws_cluster.js
+++ single_aws_cluster.js
@@ -118,6 +118,21 @@ else {*/
         res.render('newyorktimes.html',{root: dir[0]});
     });
 
+    app.get('/nytest', function(req, res){
+        var result = new WhichBrowser(req.headers);
+        console.log(result.toString());
+        if(result.isType('desktop')){
+            console.log('This is a desktop computer.');
+            deviceType = 'desktop';
+        }
+        else{
+            console.log('This is a mobile device.');
+            deviceType = 'mobile';
+        }
+
+        res.render('nyt_final.html',{root: dir[0]});
+    });
+
     app.get('/wapo', function(req, res){
         var result = new WhichBrowser(req.headers);
         console.log(result.toString());
