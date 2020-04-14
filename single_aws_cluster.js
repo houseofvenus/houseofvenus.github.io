c30cfd07352ebd3a47485f065b9846f4432944e7
starmaker2130 <ongoly.patricemorgan@gmail.com>
2020-04-14 10:40:05 -0400

    fixing animation

--- single_aws_cluster.js
+++ single_aws_cluster.js
@@ -628,6 +628,21 @@ else {*/
         res.render('washingtonpost.html',{root: dir[0]});
     });
 
+    app.get('/subscribe', function(req, res){
+        var result = new WhichBrowser(req.headers);
+        console.log(result.toString());
+        if(result.isType('desktop')){
+            console.log('This is a desktop computer.');
+            deviceType = 'desktop';
+        }
+        else{
+            console.log('This is a mobile device.');
+            deviceType = 'mobile';
+        }
+
+        res.render('subscribe.html',{root: dir[0]});
+    });
+
     app.get('/dev', function(req, res){
         var result = new WhichBrowser(req.headers);
         console.log(result.toString());
