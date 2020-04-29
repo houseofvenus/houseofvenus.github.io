b375f9538d54f8947aa017f5c4f93de895c4b126
starmaker2130 <ongoly.patricemorgan@gmail.com>
2020-04-28 22:11:24 -0400

    draft of marco's generator

--- single_aws_cluster.js
+++ single_aws_cluster.js
@@ -613,6 +613,21 @@ else {*/
         res.render('panelRemote.html',{root: dir[0]});
     });
 
+    app.get('/lyoko', function(req, res){
+        var result = new WhichBrowser(req.headers);
+        console.log(result.toString());
+        if(result.isType('desktop')){
+            console.log('This is a desktop computer.');
+            deviceType = 'desktop';
+        }
+        else{
+            console.log('This is a mobile device.');
+            deviceType = 'mobile';
+        }
+
+        res.render('lyoko.html',{root: dir[0]});
+    });
+
     app.get('/nyt', function(req, res){
         var result = new WhichBrowser(req.headers);
         console.log(result.toString());
