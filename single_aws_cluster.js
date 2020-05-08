c5060960c87efe9102f85f118e988fc0b283f307
Patrice-Morgan Ongoly <ongoly.patricemorgan@gmail.com>
2020-05-07 22:04:30 -0400

    added button functionality to access barron's immersive application; loading dynamic textures next

--- single_aws_cluster.js
+++ single_aws_cluster.js
@@ -103,6 +103,21 @@ else {*/
         res.render('index.html',{root: dir[0]});
     });
 
+    app.get('/user', function(req, res){
+        var result = new WhichBrowser(req.headers);
+        console.log(result.toString());
+        if(result.isType('desktop')){
+            console.log('This is a desktop computer.');
+            deviceType = 'desktop';
+        }
+        else{
+            console.log('This is a mobile device.');
+            deviceType = 'mobile';
+        }
+
+        res.render('user.html',{root: dir[0]});
+    });
+
     app.get('/cvm', function(req, res){
         var result = new WhichBrowser(req.headers);
         console.log(result.toString());
