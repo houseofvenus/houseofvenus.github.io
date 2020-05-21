23963c76c36c7d5c737acee29e16c063bcdbde7a
starmaker2130 <ongoly.patricemorgan@gmail.com>
2020-05-21 16:22:33 -0400

    added styles for control buttons

--- js/md-200-ux.js
+++ js/md-200-ux.js
@@ -41,43 +41,51 @@ function attachImmersiveButtonListeners(){
 
     });
     
-    document.getElementById("dpad-up-button-container").addEventListener("mousedown", event => {
-        console.log("pressing the forward button. \n accelerate vehicle in forward direction."); 
+    document.getElementById("dpad-up-button-container").addEventListener("mousedown", function(event){
+        console.log("pressing the forward button. \n accelerate vehicle in forward direction.");
+        this.style.backgroundColor = "rgba(255, 255, 255, 1.0)";
     });
     
-    document.getElementById("dpad-up-button-container").addEventListener("mouseup", event => {
+    document.getElementById("dpad-up-button-container").addEventListener("mouseup", function(event){
         console.log("released the forward button. \n pause accelerator; let tapering physics take over...");
+        this.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
     });
     
-    document.getElementById("dpad-down-button-container").addEventListener("mousedown", event => {
+    document.getElementById("dpad-down-button-container").addEventListener("mousedown", function(event){
         console.log("pressing the back button. \n accelerate vehicle in reverse direction."); 
+        this.style.backgroundColor = "rgba(255, 255, 255, 1.0)";
     });
     
-    document.getElementById("dpad-down-button-container").addEventListener("mouseup", event => {
+    document.getElementById("dpad-down-button-container").addEventListener("mouseup", function(event){
         console.log("released the back button. \n pause accelerator; let tapering physics take over...");
+        this.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
     });
     
-    document.getElementById("dpad-left-button-container").addEventListener("mousedown", event => {
+    document.getElementById("dpad-left-button-container").addEventListener("mousedown", function(event){
         console.log("pressing the left button. \n turn vehicle counter clockwise."); 
+        this.style.backgroundColor = "rgba(255, 255, 255, 1.0)";
     });
     
-    document.getElementById("dpad-left-button-container").addEventListener("mouseup", event => {
+    document.getElementById("dpad-left-button-container").addEventListener("mouseup", function(event){
         console.log("released the left button. \n let wheel return to directional equilibrium...");
+        this.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
     });
     
-    document.getElementById("dpad-right-button-container").addEventListener("mousedown", event => {
+    document.getElementById("dpad-right-button-container").addEventListener("mousedown", function(event){
         console.log("pressing the right button. \n turn vehicle clockwise."); 
+        this.style.backgroundColor = "rgba(255, 255, 255, 1.0)";
     });
     
-    document.getElementById("dpad-right-button-container").addEventListener("mouseup", event => {
+    document.getElementById("dpad-right-button-container").addEventListener("mouseup", function(event){
         console.log("released the right button. \n let wheel return to directional equilibrium...");
+        this.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
     });
     
-    document.getElementById("dpad-alpha-action-button-container").addEventListener("click", event => {
+    document.getElementById("dpad-alpha-action-button-container").addEventListener("click", function(event){
         console.log("clicked the alpha action button. \n toggle vehicle size."); 
     });
     
-    document.getElementById("dpad-beta-action-button-container").addEventListener("click", event => {
+    document.getElementById("dpad-beta-action-button-container").addEventListener("click", function(event){
         console.log("clicked the beta action button. \n use special item."); 
     });
 }
