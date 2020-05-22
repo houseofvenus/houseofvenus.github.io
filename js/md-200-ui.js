70a028dfcd34514c93402eb0a9285c32d0f6a500
starmaker2130 <ongoly.patricemorgan@gmail.com>
2020-05-21 20:12:46 -0400

    completed selection process

--- js/md-200-ui.js
+++ js/md-200-ui.js
@@ -1,18 +1,147 @@
+let directionPhase;
+let carInFocus = 0;
+let previousCarInFocus;
+let selectionArray = [
+    {
+        obj: "#bugatti-obj",
+        mtl: "#bugatti-mtl",
+        scale: "0.03 0.03 0.03",
+        visible: false,
+    },
+    {
+        obj: "#lambo-obj",
+        mtl: "#lambo-mtl",
+        scale: "3 3 3",
+        visible: false,
+    },
+    {
+        obj: "#audi-obj",
+        color: "red",
+        scale: "6 6 6",
+        visible: false,
+    }
+];
+
+let userPosition = {
+    lat: null,
+    long: null
+};
+
+function hideCarAtCurrentLocation(){
+    if(selectionArray[previousCarInFocus].visible){
+        selectionArray[previousCarInFocus].visible = false;
+    }
+    else{//toggle error checker | i.e. should not occur; if it does there is an error
+        selectionArray[previousCarInFocus].visible = true;
+    }
+    document.getElementById(`location-object-container-${previousCarInFocus}`).setAttribute("visible", selectionArray[previousCarInFocus].visible);
+}
+
+function revealCarAtCurrentLocation(lat, long){
+    if(selectionArray[carInFocus].visible){
+        selectionArray[carInFocus].visible = false;
+    }
+    else{
+        selectionArray[carInFocus].visible = true;
+        previousCarInFocus = carInFocus;
+    }
+    document.getElementById(`location-object-container-${carInFocus}`).setAttribute("visible", selectionArray[carInFocus].visible);
+    setTimeout(function(){
+        document.getElementById(`location-object-container-${carInFocus}`).setAttribute("scale", selectionArray[carInFocus].scale);
+    }, 500);
+    
+}
+
+function removeCarFromCurrentLocation(){
+    let locationObject = document.getElementById("location-object-container");
+    locationObject.parentNode.removeChild(locationObject);
+}
+
+function addCarToCurrentLocation(lat, long){
+    if(selectionArray[carInFocus].hasOwnProperty("mtl")){
+        document.getElementById("main-experience-container").insertAdjacentHTML("beforeend", `
+        <a-obj-model id="location-object-container-${carInFocus}"
+        src="${selectionArray[carInFocus].obj}" mtl="${selectionArray[carInFocus].mtl}"
+        scale="${selectionArray[carInFocus].scale}"
+        visible="${selectionArray[carInFocus].visible}"
+        animation="property: rotation; to: 0 360 0; loop: true; dur: 30000; easing: linear"
+        gps-entity-place="latitude: ${lat}; longitude: ${long};">
+        </a-obj-model>
+        `);
+    }
+    else{
+       document.getElementById("main-experience-container").insertAdjacentHTML("beforeend", `
+        <a-obj-model id="location-object-container-${carInFocus}"
+        src="${selectionArray[carInFocus].obj}" material="color: ${selectionArray[carInFocus].color};"
+        scale="${selectionArray[carInFocus].scale}"
+        visible="${selectionArray[carInFocus].visible}"
+        animation="property: rotation; to: 0 360 0; loop: true; dur: 30000; easing: linear"
+        gps-entity-place="latitude: ${lat}; longitude: ${long};">
+        </a-obj-model>
+        `); 
+    }
+    
+}
+
+function resetCarInFocus(){
+    //removeCarFromCurrentLocation();
+    hideCarAtCurrentLocation();
+    
+    setTimeout(function(){
+        revealCarAtCurrentLocation(userPosition.lat, userPosition.long);
+        //addCarToCurrentLocation(userPosition.lat, userPosition.long)
+    }, 100);
+}
+
+/*
+lambo-obj" mtl="#lambo-mtl
+
+<a-sphere id="location-object-container"
+radius="1"
+animation="property: rotation; to: 0 360 0; loop: true; dur: 30000; easing: linear"
+position="3 0 0"
+material="src: #earth-planet-object-texture"
+;"></a-sphere>
+
+*/
+
 function buildMarkup(){
     document.getElementById("main-experience-container").insertAdjacentHTML("beforeend", `<a-marker id="nyt-marker-container" type="pattern" url="../media/pattern/pattern-nyt-marker-candidate.patt">
     <a-obj-model src="#lambo-obj" mtl="#lambo-mtl" scale="1 1 1"></a-obj-model>
 </a-marker>`);
     
-    document.getElementById("main-app-container").insertAdjacentHTML("afterbegin", `<div id="controller-cover-container">
-<div id="dpad-buttons-container">
-<div id="dpad-up-button-container" class="dpad-button-container"></div>
-<div id="dpad-left-button-container" class="dpad-button-container"></div>
-<div id="dpad-right-button-container" class="dpad-button-container"></div>
-<div id="dpad-down-button-container" class="dpad-button-container"></div>
+    for(var i=0; i<selectionArray.length; i++){
+        (function(){
+            carInFocus = i;
+            addCarToCurrentLocation(39.080962, -76.979481);
+        })();
+    }
+    
+    carInFocus = 0;
+    
+    document.getElementById("main-app-container").insertAdjacentHTML("afterbegin", `
+<div id="main-app-cover-page-container">
+    <div id="app-cover-image-container"></div>
+    <p class="text-descriptor">a DIA for exploring global roadways in pursuit of the mantle of greatest driver</p>
+    <p class="text-descriptor" style="text-align: center; text-transform: uppercase;">Tap to open DIA</p>
 </div>
-<div id="dpad-action-buttons-container">
-<div id="dpad-alpha-action-button-container" class="dpad-action-button-container">a</div>
-<div id="dpad-beta-action-button-container" class="dpad-action-button-container">b</div>
+<div id="directions-0-container" class="directions-container">
+    <ol>
+        <li>Look at your marker or tap the red button to summon your car.</li>
+        <li>Use the left and right direction pad buttons to select your choice of vehicle.</li>
+        <li>Press the green button to make your choice!</li>
+    </ol>
 </div>
+<div id="controller-cover-container">
+    <div id="dpad-buttons-container">
+        <div id="dpad-up-button-container" class="dpad-button-container"></div>
+        <div id="dpad-left-button-container" class="dpad-button-container"></div>
+        <div id="dpad-right-button-container" class="dpad-button-container"></div>
+        <div id="dpad-down-button-container" class="dpad-button-container"></div>
+    </div>
+    <div id="dpad-action-buttons-container">
+        <div id="dpad-alpha-action-button-container" class="dpad-action-button-container">a</div>
+        <div id="dpad-beta-action-button-container" class="dpad-action-button-container">b</div>
+    </div>
 </div>`);
 }
--- js/md-200-ux.js
+++ js/md-200-ux.js
@@ -30,7 +30,7 @@ let MarkerGridTracker = {
 };
 
 let stopTimeNYT, timeDiffNYT, stopTimeMIT, timeDiffMIT, stopTimeHIRO, timeDiffHIRO, stopTimeUNI, timeDiffUNI;
-
+let locationModeOn = false;
 
 function attachImmersiveButtonListeners(){
     document.getElementById("nyt-marker-container").addEventListener("markerLost", function(){
@@ -42,42 +42,108 @@ function attachImmersiveButtonListeners(){
     });
     
     document.getElementById("dpad-up-button-container").addEventListener("mousedown", function(event){
-        console.log("pressing the forward button. \n accelerate vehicle in forward direction.");
+        switch(directionPhase){
+            case 0:
+                console.log("nothing to do");
+                break;
+            case 1:
+                console.log("still nothing to do");
+                break;
+            case 2:
+                console.log("pressing the forward button. \n accelerate vehicle in forward direction.");
+                break;
+            default:
+                break;
+        }
+        
         this.style.backgroundColor = "rgba(255, 255, 255, 1.0)";
     });
     
     document.getElementById("dpad-up-button-container").addEventListener("mouseup", function(event){
-        console.log("released the forward button. \n pause accelerator; let tapering physics take over...");
+        console.log("released the forward button.");// \n pause accelerator; let tapering physics take over...
         this.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
     });
     
     document.getElementById("dpad-down-button-container").addEventListener("mousedown", function(event){
-        console.log("pressing the back button. \n accelerate vehicle in reverse direction."); 
+        switch(directionPhase){
+            case 0:
+                console.log("nothing to do");
+                break;
+            case 1:
+                console.log("still nothing to do");
+                break;
+            case 2:
+                console.log("pressing the back button. \n accelerate vehicle in reverse direction."); 
+                break;
+            default:
+                break;
+        }
+        
         this.style.backgroundColor = "rgba(255, 255, 255, 1.0)";
     });
     
     document.getElementById("dpad-down-button-container").addEventListener("mouseup", function(event){
-        console.log("released the back button. \n pause accelerator; let tapering physics take over...");
+        console.log("released the back button.");// \n pause accelerator; let tapering physics take over...
         this.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
     });
     
     document.getElementById("dpad-left-button-container").addEventListener("mousedown", function(event){
-        console.log("pressing the left button. \n turn vehicle counter clockwise."); 
+        switch(directionPhase){
+            case 0:
+                console.log("initiate app first");
+                break;
+            case 1:
+                if(carInFocus>0){
+                    previousCarInFocus = carInFocus;
+                    carInFocus--;
+                    resetCarInFocus();
+                }
+                else{
+                    console.log("left most item");
+                }
+                break;
+            case 2:
+                console.log("pressing the left button. \n turn vehicle counter clockwise."); 
+                break;
+            default:
+            break;
+        }
+        
         this.style.backgroundColor = "rgba(255, 255, 255, 1.0)";
     });
     
     document.getElementById("dpad-left-button-container").addEventListener("mouseup", function(event){
-        console.log("released the left button. \n let wheel return to directional equilibrium...");
+        console.log("released the left button.");// \n let wheel return to directional equilibrium...
         this.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
     });
     
     document.getElementById("dpad-right-button-container").addEventListener("mousedown", function(event){
-        console.log("pressing the right button. \n turn vehicle clockwise."); 
+        switch(directionPhase){
+            case 0:
+                console.log("initiate app first");
+                break;
+            case 1:
+                if(carInFocus<2){
+                    previousCarInFocus = carInFocus;
+                    carInFocus++;
+                    resetCarInFocus();
+                }
+                else{
+                    console.log("left most item");
+                }
+                break;
+            case 2:
+                console.log("pressing the right button. \n turn vehicle clockwise."); 
+                break;
+            default:
+            break;
+        }
+        
         this.style.backgroundColor = "rgba(255, 255, 255, 1.0)";
     });
     
     document.getElementById("dpad-right-button-container").addEventListener("mouseup", function(event){
-        console.log("released the right button. \n let wheel return to directional equilibrium...");
+        console.log("released the right button.");  //\n let wheel return to directional equilibrium...
         this.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
     });
     
@@ -86,6 +152,35 @@ function attachImmersiveButtonListeners(){
     });
     
     document.getElementById("dpad-beta-action-button-container").addEventListener("click", function(event){
-        console.log("clicked the beta action button. \n use special item."); 
+        console.log("clicked the beta action button. \n use special item.");
+        
+        if(locationModeOn){
+            if(directionPhase===0){
+                removeCarFromCurrentLocation();
+            }
+            locationModeOn = false;
+        }else{
+            if(directionPhase===0){
+                directionPhase = 1;
+                revealCarAtCurrentLocation(39.080962, -76.979481);
+            }
+            locationModeOn = true;
+        }
+    });
+    
+    document.getElementById("main-app-cover-page-container").addEventListener("click", function(){
+        let cover = this;
+        
+        directionPhase = 0;
+        
+        userPosition.lat = 39.080962;
+        userPosition.long = -76.979481;
+        
+        cover.style.height = 0;
+        cover.style.opacity = 0;
+        
+        setTimeout(function(){
+            cover.style.display = "none";
+        }, 500);
     });
 }
