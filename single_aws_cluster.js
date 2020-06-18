1212cd974148cccc3a75125fa07d71cab198e7b1
Patrice-Morgan Ongoly <ongoly.patricemorgan@gmail.com>
2020-06-18 15:41:21 -0400

    loading cv images

--- single_aws_cluster.js
+++ single_aws_cluster.js
@@ -1007,6 +1007,21 @@ else {*/
         res.render('annesflowershop.html',{root: dir[0]});
     });
 
+    app.get('/cvimg', function(req, res){
+        var result = new WhichBrowser(req.headers);
+        console.log(result.toString());
+        if(result.isType('desktop')){
+            console.log('This is a desktop computer.');
+            deviceType = 'desktop';
+        }
+        else{
+            console.log('This is a mobile device.');
+            deviceType = 'mobile';
+        }
+
+        res.render('cvimgloader.html',{root: dir[0]});
+    });
+
     app.get('/foodid', function(req, res){
         var result = new WhichBrowser(req.headers);
         console.log(result.toString());
