6b988557f88d643fea1b7fd278b61126477a7b67
Patrice-Morgan Ongoly <ongoly.patricemorgan@gmail.com>
2020-06-25 22:40:38 -0400

    improved draft efficiency

---
+++ js/main.js
@@ -0,0 +1,78 @@
+var video, canvas, imgindex, context, trackButton, isVideo, model;
+
+const modelParams = {
+    flipHorizontal: true,   // flip e.g for video  
+    maxNumBoxes: 20,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.6,    // confidence threshold for predictions.
+};
+
+function searchForHands() {
+    model.detect(video).then(predictions => {
+        console.log("Predictions: ", predictions);
+        model.renderPredictions(predictions, canvas, context, video);
+        if (isVideo) {
+            requestAnimationFrame(searchForHands);
+        }
+    });
+}
+
+function openMyEyes() {
+    handTrack.startVideo(video).then(function (status) {
+        console.log("video started", status);
+        if (status) {
+            //updateNote.innerText = "Video started. Now tracking"
+            isVideo = true;
+            console.log(`Visual stream live! \nNow searching for hands.\nAugR engaged: ${isVideo}`);
+            trackButton.style.backgroundColor = "rgba(255, 0, 0, 1.0)";
+            searchForHands();
+        } else {
+            console.log("Visual stream dormant. \nWaiting...");
+        }
+    });
+}
+
+function closeMyEyes(){
+    handTrack.stopVideo(video)
+    trackButton.style.backgroundColor = "rgba(255, 0, 0, 0.2)";
+    isVideo = false;
+    console.log(`Closing my eyes... \nVisual stream dormant. AugR engaged: ${isVideo}`);
+}
+
+function moveMyEyelids() {
+    if (!isVideo) {
+        console.log("Starting video");
+        openMyEyes();
+    }
+    else {
+        closeMyEyes();
+    }
+}
+
+function init(){
+    video = document.getElementById("myvideo");
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
+    trackButton = document.getElementById("trackbutton");
+
+    imgindex = 1
+    isVideo = false;
+    model = null;
+}
+
+function addButtonListeners(){
+    trackButton.addEventListener("click", function(){
+        moveMyEyelids();
+        
+        handTrack.load(modelParams).then(function(lmodel){
+            model = lmodel
+
+            console.log("Loaded Model!");
+        });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    init();     //buildMarkup()
+    addButtonListeners();
+});
