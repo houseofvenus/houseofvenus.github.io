d4b16ca4ffa72249547744c1c4e54e933294012d
Patrice-Morgan Ongoly <ongoly.patricemorgan@gmail.com>
2020-06-30 08:25:09 -0400

    updated products and vision pages

--- single_aws_cluster.js
+++ single_aws_cluster.js
@@ -104,6 +104,22 @@ else {*/
         res.render('index.html',{root: dir[0]});
     });
 
+    app.get('/products', function(req, res){
+        var result = new WhichBrowser(req.headers);
+        console.log(result.toString());
+        if(result.isType('desktop')){
+            console.log('This is a desktop computer.');
+            deviceType = 'desktop';
+        }
+        else{
+            console.log('This is a mobile device.');
+            deviceType = 'mobile';
+        }
+
+        res.render('products.html',{root: dir[0]});
+    });
+
+
     app.get('/origin', function(req, res){
         var result = new WhichBrowser(req.headers);
         console.log(result.toString());
@@ -118,6 +134,22 @@ else {*/
 
         res.render('origin.html',{root: dir[0]});
     });
+
+    app.get('/vision', function(req, res){
+        var result = new WhichBrowser(req.headers);
+        console.log(result.toString());
+        if(result.isType('desktop')){
+            console.log('This is a desktop computer.');
+            deviceType = 'desktop';
+        }
+        else{
+            console.log('This is a mobile device.');
+            deviceType = 'mobile';
+        }
+
+        res.render('vision.html',{root: dir[0]});
+    });
+
 
     app.get('/clavier', function(req, res){
         var result = new WhichBrowser(req.headers);
