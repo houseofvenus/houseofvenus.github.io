4ac4f6f2226c83d4cfd5384d7c24646cf7122d05
Patrice-Morgan Ongoly <ongoly.patricemorgan@gmail.com>
2020-06-29 23:47:17 -0400

    adjusting origins page

--- single_aws_cluster.js
+++ single_aws_cluster.js
@@ -104,6 +104,21 @@ else {*/
         res.render('index.html',{root: dir[0]});
     });
 
+    app.get('/origin', function(req, res){
+        var result = new WhichBrowser(req.headers);
+        console.log(result.toString());
+        if(result.isType('desktop')){
+            console.log('This is a desktop computer.');
+            deviceType = 'desktop';
+        }
+        else{
+            console.log('This is a mobile device.');
+            deviceType = 'mobile';
+        }
+
+        res.render('origin.html',{root: dir[0]});
+    });
+
     app.get('/clavier', function(req, res){
         var result = new WhichBrowser(req.headers);
         console.log(result.toString());
