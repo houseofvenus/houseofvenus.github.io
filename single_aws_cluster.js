606ece15eb5633459bd499c8f904aaafcd690756
starmaker2130 <ongoly.patricemorgan@gmail.com>
2020-07-17 12:01:23 -0400

    add user pages

--- single_aws_cluster.js
+++ single_aws_cluster.js
@@ -104,6 +104,36 @@ else {*/
         res.render('index.html',{root: dir[0]});
     });
 
+    app.get('/pARk/connect', function(req, res){
+        var result = new WhichBrowser(req.headers);
+        console.log(result.toString());
+        if(result.isType('desktop')){
+            console.log('This is a desktop computer.');
+            deviceType = 'desktop';
+        }
+        else{
+            console.log('This is a mobile device.');
+            deviceType = 'mobile';
+        }
+
+        res.render('connect.html',{root: dir[0]});
+    });
+
+    app.get('/pARk/vaibhav', function(req, res){
+        var result = new WhichBrowser(req.headers);
+        console.log(result.toString());
+        if(result.isType('desktop')){
+            console.log('This is a desktop computer.');
+            deviceType = 'desktop';
+        }
+        else{
+            console.log('This is a mobile device.');
+            deviceType = 'mobile';
+        }
+
+        res.render('vaibhav.html',{root: dir[0]});
+    });
+
     app.get('/products', function(req, res){
         var result = new WhichBrowser(req.headers);
         console.log(result.toString());
