52c48dd1d11805e9c187e8d0e4e0e5ab7e77d495
starmaker2130 <ongoly.patricemorgan@gmail.com>
2020-09-18 18:11:40 -0400

    upgraded stream page ui

--- single_aws_cluster.js
+++ single_aws_cluster.js
@@ -119,6 +119,21 @@ else {*/
         res.render('nexus.html',{root: dir[0]});
     });
 
+    app.get('/nexus/stream', function(req, res){
+        var result = new WhichBrowser(req.headers);
+        console.log(result.toString());
+        if(result.isType('desktop')){
+            console.log('This is a desktop computer.');
+            deviceType = 'desktop';
+        }
+        else{
+            console.log('This is a mobile device.');
+            deviceType = 'mobile';
+        }
+
+        res.render('nexus-stream.html',{root: dir[0]});
+    });
+
     app.get('/', function(req, res){
         var result = new WhichBrowser(req.headers);
         console.log(result.toString());
